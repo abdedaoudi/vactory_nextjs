49e5ea1364a1d6289ffa37e39e98e1552d1112a4
abdedaoudi <a.daoudi@void.fr>
2022-03-24 18:18:50 +0100

    feat(core): fixing slider

--- packages/ui/src/slider/fullBackgroundSlider.js
+++ packages/ui/src/slider/fullBackgroundSlider.js
@@ -2,22 +2,21 @@ import React from "react"
 //import { Button } from "@vactory/ui/button"
 import { Link } from "@vactory/ui/link"
 
-export const FullBackgroundSlider = (isActive) => {
+export const FullBackgroundSlider = (props) => {
+	const isActive = props.isActive
 	return (
-		<div className="keen-slider__slide number-slide bg-gray-600 flex items-center justify-center text-5xl text-white font-medium h-screen max-h-screen">
+		<div className="keen-slider__slide bg-cover number-slide bg-gray-600 relative items-center justify-center text-5xl text-white font-medium h-screen w-full max-h-screen">
 			{isActive && (
-				<div className="flex h-full flex-col justify-center items-center mx-auto max-w-lg space-x-3">
+				<div className="flex h-full flex-col justify-center items-center  space-x-3">
 					<div className="text-center" textAlign="center">
 						{console.log("isActive")}
 						Desc
 						<div className="flex justify-center mt-12">
-							<Link href={"https://keen-slider.io/docs"}>
-								<button
-									type="button"
-									className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-								>
-									Button text
-								</button>
+							<Link
+								className="inline-flex items-center px-3 py-2  border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-transparent border-2 border-white hover:bg-white hover:text-black  focus:outline-none focus:ring-2 focus:ring-offset-2 "
+								href={"https://keen-slider.io/docs"}
+							>
+								En savoir plus
 							</Link>
 						</div>
 					</div>
--- packages/ui/src/slider/slider.js
+++ packages/ui/src/slider/slider.js
@@ -2,27 +2,37 @@ import React, { useState } from "react"
 import "keen-slider/keen-slider.min.css"
 import { useKeenSlider } from "keen-slider/react.es" // import from 'keen-slider/react.es' for to get an ES module
 //keep this css file just for the icon next and previous to change them after
-import "./styles.css"
+//import "./styles.css"
 import { FullBackgroundSlider } from "./fullBackgroundSlider"
 
-export const Slider = () => {
+export const Slider = (props) => {
 	const [currentSlide, setCurrentSlide] = useState(0)
 	const [loaded, setLoaded] = useState(false)
 	const [sliderRef, instanceRef] = useKeenSlider({
 		initial: 0,
+		loop: true,
+		mode: "free-snap",
+		renderMode: "performance",
+		defaultAnimation: {
+			duration: 10000,
+		},
 		slideChanged(slider) {
 			setCurrentSlide(slider.track.details.rel)
 		},
 		created() {
 			setLoaded(true)
 		},
+		rtl: true,
 	})
 
 	const ns = [1, 2, 3, 4]
 	return (
 		<>
-			<div className="navigation-wrapper">
-				<div ref={sliderRef} className="keen-slider">
+			<div
+				className="navigation-wrapper relative h-full w-full overflow-hidden
+"
+			>
+				<div ref={sliderRef} className="keen-slider relative w-full">
 					{ns.map((n, index) => {
 						return <FullBackgroundSlider key={n} isActive={currentSlide === index} />
 					})}
@@ -45,7 +55,7 @@ export const Slider = () => {
 					</>
 				)}
 				{loaded && instanceRef.current && (
-					<div className="dots">
+					<div className="dots flex justify-center py-2.5 bottom-10 w-full ">
 						{[...Array(instanceRef.current.track.details.slides.length).keys()].map(
 							(idx) => {
 								return (
@@ -54,7 +64,10 @@ export const Slider = () => {
 										onClick={() => {
 											instanceRef.current?.moveToIdx(idx)
 										}}
-										className={"dot" + (currentSlide === idx ? " active" : "")}
+										className={
+											"dot w-2 h-2 bg-gray-400 p-1 cursor-pointer border-current rounded-full my-0 mx-1.5 focus:outline-none" +
+											(currentSlide === idx ? " active bg-black" : "")
+										}
 									></button>
 								)
 							}
@@ -69,16 +82,28 @@ export const Slider = () => {
 function Arrow(props) {
 	const disabeld = props.disabled ? " arrow--disabled" : ""
 	return (
-		<svg
+		<button
 			onClick={props.onClick}
-			className={`arrow ${props.left ? "arrow--left" : "arrow--right"} ${disabeld}`}
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 24 24"
+			className={`arrow -translate-y-1/2 fill-white absolute bottom-1/2 items-center cursor-pointer${
+				props.left
+					? "arrow--left left-20 border-2 border-white hover:bg-yellow-400  hover:border-transparent py-5 px-5 rounded-full"
+					: "arrow--right right-20 border-2 border-white hover:bg-yellow-400 hover:border-transparent py-5 px-5 rounded-full"
+			} ${disabeld}`}
 		>
-			{props.left && (
-				<path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-			)}
-			{!props.left && <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />}
-		</svg>
+			<i className="icon-caret-left h-4 w-4 before:">
+				<svg
+					className="arrows relative top-0"
+					height="15"
+					viewBox="0 0 30 30"
+					width="15"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					{props.left && (
+						<path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+					)}
+					{props.right && <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />}
+				</svg>
+			</i>
+		</button>
 	)
 }
