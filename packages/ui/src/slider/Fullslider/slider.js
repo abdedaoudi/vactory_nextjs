37a4a2874d9cb7f0ffc89765fdd4ac07444a69d3
abdedaoudi <a.daoudi@void.fr>
2022-03-31 19:20:38 +0200

    feat(core): fix rtl sliders

--- packages/ui/src/slider/Fullslider/slider.js
+++ packages/ui/src/slider/Fullslider/slider.js
@@ -52,7 +52,6 @@ export const Slider = (items) => {
 						<>
 							<Arrow
 								left
-								ltr
 								onClick={(e) => {
 									instanceRef.current?.prev()
 								}}
@@ -96,34 +95,31 @@ export const Slider = (items) => {
 
 function Arrow(props) {
 	return (
-		props.ltr ? true : false,
-		(
-			<button
-				onClick={props.onClick}
-				className={`arrow sm:translate-y-1/2 items-center justify-center bottom-10 fill-white absolute sm:bottom-1/2  sm:cursor-pointer  ltr:right-6  rtl:left-6 sm:border sm:border-white focus:sm:border-0 focus:bg focus:bg-yellow-400 hover:bg-white sm:hover:bg-yellow-400  hover:border-transparent py-4 px-4 sm:rounded-full  sm:transition sm:ease-in-out sm:delay-200 sm:hover:-translate-x-1 sm:hover:scale-110 w-12 h-12  ${
-					props.left
-						? "arrow--left ltr:sm:left-20 rtl:sm:right-20 ltr:mr-12 rtl:ml-12 bg-white sm:bg-transparent "
-						: "arrow--right ltr:sm:right-20 rtl:sm:left-20"
-				}`}
-			>
-				<i className="relative transition hover:-translate-x-1 hover:scale-110  ">
-					<svg
-						className="sm:fill-white fill-black rtl:transform rtl:rotate-180"
-						viewBox="0 0 30 30"
-						xmlns="http://www.w3.org/2000/svg"
-						height="15"
-						width="15"
-					>
-						{props.left && (
-							<path d="M13,26a1,1,0,0,1-.71-.29l-9-9a1,1,0,0,1,0-1.42l9-9a1,1,0,1,1,1.42,1.42L5.41,16l8.3,8.29a1,1,0,0,1,0,1.42A1,1,0,0,1,13,26Z" />
-						)}
-						{props.right && (
-							<path d="M19,26a1,1,0,0,1-.71-.29,1,1,0,0,1,0-1.42L26.59,16l-8.3-8.29a1,1,0,0,1,1.42-1.42l9,9a1,1,0,0,1,0,1.42l-9,9A1,1,0,0,1,19,26Z" />
-						)}
-						<path d="M28,17H4a1,1,0,0,1,0-2H28a1,1,0,0,1,0,2Z" />
-					</svg>
-				</i>
-			</button>
-		)
+		<button
+			onClick={props.onClick}
+			className={`arrow sm:translate-y-1/2 items-center justify-center bottom-10 fill-white absolute sm:bottom-1/2  sm:cursor-pointer  ltr:right-6  rtl:left-6 sm:border sm:border-white focus:sm:border-0 focus:bg focus:bg-yellow-400 hover:bg-white sm:hover:bg-yellow-400  hover:border-transparent py-4 px-4 sm:rounded-full  sm:transition sm:ease-in-out sm:delay-200 sm:hover:-translate-x-1 sm:hover:scale-110 w-12 h-12  ${
+				props.left
+					? "arrow--left ltr:sm:left-20 rtl:sm:right-20 ltr:mr-12 rtl:ml-12 bg-white sm:bg-transparent "
+					: "arrow--right ltr:sm:right-20 rtl:sm:left-20"
+			}`}
+		>
+			<i className="relative transition hover:-translate-x-1 hover:scale-110  ">
+				<svg
+					className="sm:fill-white fill-black rtl:transform rtl:rotate-180"
+					viewBox="0 0 30 30"
+					xmlns="http://www.w3.org/2000/svg"
+					height="15"
+					width="15"
+				>
+					{props.left && (
+						<path d="M13,26a1,1,0,0,1-.71-.29l-9-9a1,1,0,0,1,0-1.42l9-9a1,1,0,1,1,1.42,1.42L5.41,16l8.3,8.29a1,1,0,0,1,0,1.42A1,1,0,0,1,13,26Z" />
+					)}
+					{props.right && (
+						<path d="M19,26a1,1,0,0,1-.71-.29,1,1,0,0,1,0-1.42L26.59,16l-8.3-8.29a1,1,0,0,1,1.42-1.42l9,9a1,1,0,0,1,0,1.42l-9,9A1,1,0,0,1,19,26Z" />
+					)}
+					<path d="M28,17H4a1,1,0,0,1,0-2H28a1,1,0,0,1,0,2Z" />
+				</svg>
+			</i>
+		</button>
 	)
 }
